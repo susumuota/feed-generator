0684254cf2202cc075878fdfc6cbdb718db13fc0
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-07-16 02:57:32 +0900

    feat: add Tech News feed.

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,8 +5,10 @@ import {
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
 type RuleType = {
-  include: RegExp | null
-  exclude: RegExp | null
+  includeAuthors: string[] | null
+  excludeAuthors: string[] | null
+  includeText: RegExp | null
+  excludeText: RegExp | null
   feed: string
   metric: string
   rating: number
@@ -15,27 +17,53 @@ type RuleType = {
 
 const RULES: RuleType[] = [
   {
-    include:
+    includeAuthors: null,
+    excludeAuthors: null,
+    includeText:
       /\bLLMs?\b|language model|言語モデル|transformer model|transformer architecture|self[-\s]attention|gpt[-\s]?4|gpt[-\s]?3\.5|\banthropic\b|hugging\s?face|vicuna|guanaco|wizardlm|airoboros|qlora|ggml|gptq|llama\.cpp|fastchat|gpt4all|langchain|llama[_\s]?index|autogpt|babyagi/i,
-    exclude: null,
+    excludeText: null,
     feed: 'whats-llm',
     metric: 'RegExp',
     rating: 5,
     explanation: '',
   },
   {
-    include: /chat\s?gpt|\bGPT\b|openai/i,
-    exclude: /Summary by GPT/,
+    includeAuthors: null,
+    excludeAuthors: null,
+    includeText: /chat\s?gpt|\bGPT\b|openai/i,
+    excludeText: /Summary by GPT/,
     feed: 'whats-gpt',
     metric: 'RegExp',
     rating: 5,
     explanation: '',
   },
   {
-    include: /generative ai|生成系?\s?AI/i,
-    exclude: null,
+    includeAuthors: null,
+    excludeAuthors: null,
+    includeText: /generative ai|gen ai|生成系?\s?AI/i,
+    excludeText: null,
     feed: 'whats-gen-ai',
     metric: 'RegExp',
+    rating: 5,
+    explanation: '',
+  },
+  {
+    // convert from handle to did
+    // https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=newsycombinator.bsky.social
+    includeAuthors: [
+      'did:plc:7dh44snmqoa4gyzv3652gm3j', // newsycombinator.bsky.social // cspell:disable-line
+      'did:plc:apeaukvxm3yedgqw5zcf5pwc', // hacker-news-jp.bsky.social // cspell:disable-line
+      'did:plc:eidn2o5kwuaqcss7zo7ivye5', // github-trending.bsky.social // cspell:disable-line
+      'did:plc:ppuqidjyabv5iwzeoxt4fq5o', // github-trending-js.bsky.social // cspell:disable-line
+      'did:plc:tlhqo7uw5d3rcohosg3io7t5', // hatena-tech.bsky.social // cspell:disable-line
+      'did:plc:vtpyqvwce4x6gpa5dcizqecy', // techcrunch.bsky.social // cspell:disable-line
+      'did:plc:z5xxhxqv6elnjzulyf7t22wk', // paper.bsky.social // cspell:disable-line
+    ],
+    excludeAuthors: null,
+    includeText: null,
+    excludeText: null,
+    feed: 'tech-news',
+    metric: 'RegExp', // TODO: consider the metric
     rating: 5,
     explanation: '',
   },
@@ -55,13 +83,28 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = RULES.map(
-      ({ include, exclude, feed, metric, rating, explanation }) =>
+      ({
+        includeAuthors,
+        excludeAuthors,
+        includeText,
+        excludeText,
+        feed,
+        metric,
+        rating,
+        explanation,
+      }) =>
         ops.posts.creates
           .filter((create) =>
-            include ? include.test(create.record.text) : true,
+            includeAuthors ? includeAuthors.includes(create.author) : true,
           )
           .filter((create) =>
-            exclude ? !exclude.test(create.record.text) : true,
+            excludeAuthors ? !excludeAuthors.includes(create.author) : true,
+          )
+          .filter((create) =>
+            includeText ? includeText.test(create.record.text) : true,
+          )
+          .filter((create) =>
+            excludeText ? !excludeText.test(create.record.text) : true,
           )
           .map((create) => ({
             uri: create.uri,
