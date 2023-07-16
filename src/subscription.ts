8976294d20db5bff2e5c8a2d89659ecb2cd61ee4
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-07-17 00:11:11 +0900

    chore: replace string[] to RegExp.

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,8 +5,8 @@ import {
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
 type RuleType = {
-  includeAuthors: string[] | null
-  excludeAuthors: string[] | null
+  includeAuthor: RegExp | null
+  excludeAuthor: RegExp | null
   includeText: RegExp | null
   excludeText: RegExp | null
   feed: string
@@ -17,8 +17,8 @@ type RuleType = {
 
 const RULES: RuleType[] = [
   {
-    includeAuthors: null,
-    excludeAuthors: null,
+    includeAuthor: null,
+    excludeAuthor: null,
     // prettier-ignore
     includeText: new RegExp(
       '\bLLMs?\b|language model|言語モデル|transformer model|transformer architecture|self[-\s]attention|gpt[-\s]?4|gpt[-\s]?3\.5|\banthropic\b|hugging\s?face|vicuna|guanaco|wizardlm|airoboros|qlora|ggml|gptq|llama\.cpp|fastchat|gpt4all|langchain|llama[_\s]?index|autogpt|babyagi',
@@ -31,8 +31,8 @@ const RULES: RuleType[] = [
     explanation: '',
   },
   {
-    includeAuthors: null,
-    excludeAuthors: null,
+    includeAuthor: null,
+    excludeAuthor: null,
     // prettier-ignore
     includeText: new RegExp('chat\s?gpt|\bGPT\b|openai', 'i'),
     excludeText: new RegExp('Summary by GPT', 'i'),
@@ -42,8 +42,8 @@ const RULES: RuleType[] = [
     explanation: '',
   },
   {
-    includeAuthors: null,
-    excludeAuthors: null,
+    includeAuthor: null,
+    excludeAuthor: null,
     // prettier-ignore
     includeText: new RegExp('generative ai|gen ai|生成系?\s?AI', 'i'),
     excludeText: null,
@@ -55,17 +55,19 @@ const RULES: RuleType[] = [
   {
     // convert from handle to did
     // https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=newsycombinator.bsky.social
-    includeAuthors: [
-      'did:plc:7dh44snmqoa4gyzv3652gm3j', // newsycombinator.bsky.social // cspell:disable-line
-      'did:plc:apeaukvxm3yedgqw5zcf5pwc', // hacker-news-jp.bsky.social // cspell:disable-line
-      'did:plc:eidn2o5kwuaqcss7zo7ivye5', // github-trending.bsky.social // cspell:disable-line
-      'did:plc:ppuqidjyabv5iwzeoxt4fq5o', // github-trending-js.bsky.social // cspell:disable-line
-      'did:plc:tlhqo7uw5d3rcohosg3io7t5', // hatena-tech.bsky.social // cspell:disable-line
-      'did:plc:vtpyqvwce4x6gpa5dcizqecy', // techcrunch.bsky.social // cspell:disable-line
-      'did:plc:z5xxhxqv6elnjzulyf7t22wk', // paper.bsky.social // cspell:disable-line
-      'did:plc:pv7fudnt4dspurzdnyq73pfe', // techmeme.com // cspell:disable-line
-    ],
-    excludeAuthors: null,
+    // 'did:plc:7dh44snmqoa4gyzv3652gm3j', // newsycombinator.bsky.social // cspell:disable-line
+    // 'did:plc:apeaukvxm3yedgqw5zcf5pwc', // hacker-news-jp.bsky.social // cspell:disable-line
+    // 'did:plc:eidn2o5kwuaqcss7zo7ivye5', // github-trending.bsky.social // cspell:disable-line
+    // 'did:plc:ppuqidjyabv5iwzeoxt4fq5o', // github-trending-js.bsky.social // cspell:disable-line
+    // 'did:plc:tlhqo7uw5d3rcohosg3io7t5', // hatena-tech.bsky.social // cspell:disable-line
+    // 'did:plc:vtpyqvwce4x6gpa5dcizqecy', // techcrunch.bsky.social // cspell:disable-line
+    // 'did:plc:z5xxhxqv6elnjzulyf7t22wk', // paper.bsky.social // cspell:disable-line
+    // 'did:plc:pv7fudnt4dspurzdnyq73pfe', // techmeme.com // cspell:disable-line
+    includeAuthor: new RegExp(
+      'did:plc:7dh44snmqoa4gyzv3652gm3j|did:plc:apeaukvxm3yedgqw5zcf5pwc|did:plc:eidn2o5kwuaqcss7zo7ivye5|did:plc:ppuqidjyabv5iwzeoxt4fq5o|did:plc:tlhqo7uw5d3rcohosg3io7t5|did:plc:vtpyqvwce4x6gpa5dcizqecy|did:plc:z5xxhxqv6elnjzulyf7t22wk|did:plc:pv7fudnt4dspurzdnyq73pfe', // cspell:disable-line
+      'i',
+    ),
+    excludeAuthor: null,
     includeText: null,
     excludeText: null,
     feed: 'tech-news',
@@ -83,8 +85,8 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = RULES.map(
       ({
-        includeAuthors,
-        excludeAuthors,
+        includeAuthor,
+        excludeAuthor,
         includeText,
         excludeText,
         feed,
@@ -94,10 +96,10 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
       }) =>
         ops.posts.creates
           .filter((create) =>
-            includeAuthors ? includeAuthors.includes(create.author) : true,
+            includeAuthor ? includeAuthor.test(create.author) : true,
           )
           .filter((create) =>
-            excludeAuthors ? !excludeAuthors.includes(create.author) : true,
+            excludeAuthor ? !excludeAuthor.test(create.author) : true,
           )
           .filter((create) =>
             includeText ? includeText.test(create.record.text) : true,
