487b09b8c95a9631fd2a67992b1b452fe6508282
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-07-28 17:29:56 +0900

    feat: add ChatGPT feed.

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -5,6 +5,7 @@ import {
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as whatsLlm from './whats-llm'
 import * as whatsLlmRaw from './whats-llm-raw'
+import * as whatsChatgpt from './whats-chatgpt'
 import * as techNews from './tech-news'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
@@ -12,6 +13,7 @@ type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 const algos: Record<string, AlgoHandler> = {
   [whatsLlm.shortname]: whatsLlm.handler,
   [whatsLlmRaw.shortname]: whatsLlmRaw.handler,
+  [whatsChatgpt.shortname]: whatsChatgpt.handler,
   [techNews.shortname]: techNews.handler,
 }
 
